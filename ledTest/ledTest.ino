a88468fce405e5a201278bc0c8ce2a7f4addab23
JaimeNufio <jaimeenufio@gmail.com>
2017-06-09 17:41:44 -0400

    First commit

---
+++ ledTest/ledTest.ino
@@ -0,0 +1,17 @@
+int ledPin = 13;
+//Define the pins.
+
+void setup(){
+  pinMode(ledPin,OUTPUT);
+  //pinMode defined
+}
+
+void loop(){
+  digitalWrite(ledPin,HIGH);
+  delay(2000);
+digitalWrite(ledPin,LOW);
+  delay(2000);
+
+  //how to change modes
+}
+
