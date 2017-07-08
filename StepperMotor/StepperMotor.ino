ac90a23f7208d8b8495f1de81fdc6d4f611c366e
JaimeNufio <jaimeenufio@gmail.com>
2017-07-08 01:59:19 -0400

    Backup Before Windows Reinstall

--- StepperMotor/StepperMotor.ino
+++ StepperMotor/StepperMotor.ino
@@ -1,39 +1,67 @@
+//28byj-48
+
 int pinA = 7;
 int pinB = 6;
 int pinC = 5;
 int pinD = 4;
 int pins[] = {pinA,pinB,pinC,pinD};
-int pinsOpp[] = {pinC,pinD,pinA,pinB};
+int pinsOpp[] = {pinD,pinC,pinB,pinA};
 int motorDelay = 5;
+int AnalogY = A0;
 int AnalogX = A1;
+int Button = 2;
+int analogXVal = 0;
+
+bool dir = true;
+bool on = true;
 
 void setup(){
+  Serial.begin(9600);
   pinMode(pinA,OUTPUT);
   pinMode(pinB,OUTPUT);
   pinMode(pinC,OUTPUT);
   pinMode(pinD,OUTPUT);
+  pinMode(Button,INPUT);
 }
 void loop(){
-  motorDelay = analogRead(AnalogX);
+  analogXVal = analogRead(AnalogX);
+  if (analogXVal > 550){
+    dir = true;
+  }else if (analogXVal < 400){
+    dir = false;
+  }
+
+  Serial.println(analogXVal);
+
   //singleExcite(1);
-  twoStep(1);
+  twoStep(1,dir,on);
 }
 
-void twoStep(int dTime){
-  int current = 0;
-  int next = 0;
-  for (int i = 0; i<4;i++){
-    current = i;
-    if (current == 3){
-      next = 0;
-    }else{
-      next = current+1;
+void twoStep(int dTime, bool forward, bool on){
+  if (on){
+    int current = 0;
+    int next = 0;
+    for (int i = 0; i<4;i++){
+      current = i;
+      if (current == 3){
+        next = 0;
+      }else{
+        next = current+1;
+      }
+      if (forward){
+        digitalWrite(pins[current],HIGH);
+        delay(dTime);
+        digitalWrite(pins[next],HIGH);
+        delay(dTime);
+        digitalWrite(pins[current],LOW);
+      }else{
+        digitalWrite(pinsOpp[current],HIGH);
+        delay(dTime);
+        digitalWrite(pinsOpp[next],HIGH);
+        delay(dTime);
+        digitalWrite(pinsOpp[current],LOW);    
+      }
     }
-    digitalWrite(pins[current],HIGH);
-    delay(dTime);
-    digitalWrite(pins[next],HIGH);
-    delay(dTime);
-    digitalWrite(pins[current],LOW);
   }
 }
 
